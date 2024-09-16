9d17f7e892a36bca88b60e8f603d27c6654167a8
Woodn-haha <64898037+Woodn-haha@users.noreply.github.com>
2024-09-17 00:00:22 +0800

    Add files via upload

---
+++ getATcontent.py
@@ -0,0 +1,45 @@
+################################################################
+#@author: Haidan Wu                       
+#                                         
+#Calculate AU content                     
+#Usage: python getAT content.py fasta.file > AT_content.output 
+################################################################
+
+import sys
+
+fastaFile = open(sys.argv[1], 'r')
+
+gc = 0
+at = 0
+unknown = 0
+
+for line in fastaFile:
+    if line.startswith('>'):
+        if(gc + at) > 0:
+            total = gc + at
+            total2 = total + unknown
+            percentage = round((float(at / total2))*100, 3 )
+            print (seq_id,percentage)
+        seq_id = line.strip()[1:]
+
+        #reset counts
+        gc = 0
+        at = 0
+        unknown = 0
+
+    else:
+        nuc_str = list(line.strip())
+        for n in nuc_str:
+            if n == 'G' or n == 'g' or n== 'C' or n == 'c':
+                gc += 1
+            elif n== 'A' or n =='a' or n == 'T' or n == 't':
+                at += 1
+            elif n== 'N' or n=='n':
+                unknown += 1
+
+#calculates for last sequence in file
+total = gc +at
+total2 = total + unknown
+percent = round((float(at / total2))*100, 3 ) 
+print (seq_id,percent)
+
