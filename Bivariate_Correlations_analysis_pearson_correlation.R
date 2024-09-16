665b93887e0395db08b05e27da86af979b3415f7
Woodn-haha <64898037+Woodn-haha@users.noreply.github.com>
2024-09-17 00:04:25 +0800

    Add files via upload

---
+++ Bivariate_Correlations_analysis_pearson_correlation.R
@@ -0,0 +1,21 @@
+###########################################
+#                                         #
+#Pearson correlation coefficient          #
+#Bivariate Correlations analysis          #
+###########################################
+
+library(ggplot2)
+library(ggpubr)
+library(ggstatsplot)
+options(stringsAsFactors=F)
+
+
+DR1 <-read.table("input.txt",header = T,sep="\t")
+cor.test(DR1$variables1,DR1$variables2, method = "pearson")
+
+
+pdf("output2.pdf", width = 5.0, height = 3.2)
+ggscatterstats(DR1, x = "variables1",
+               y = "variables2", 
+               title = "Title")
+dev.off()
